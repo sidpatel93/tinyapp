f5e21d985b8d5f6dffe83f588c2c9d5d5920f4ef
spatel93 <sid.auburn@gmail.com>
2021-11-08 15:08:05 +0000

    use inbuilt express body parser to get the post request data

--- express_server.js
+++ express_server.js
@@ -1,10 +1,12 @@
 const express = require("express");
+//const bodyParser = require('body-parser')
+
 const app = express();
 const PORT = 8080; // default port 8080
 
 app.set("view engine", "ejs")
-
-
+//app.use (bodyParser.urlencoded({extended: true}))
+app.use(express.urlencoded({extended: true}))
 
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
@@ -19,12 +21,10 @@ app.get("/urls.json", (req, res) => {
   res.json(urlDatabase);
 });
 
-app.get("/urls", (req, res) => {
-  const templateVars = {
-    urls: urlDatabase 
-  }
-  res.render("urls_index", templateVars)
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
 });
+
 
 app.get("/urls/:shortURL", (req, res) => {
   const templateVars = {
@@ -32,6 +32,18 @@ app.get("/urls/:shortURL", (req, res) => {
     longURL: urlDatabase[req.params.shortURL] 
   }
   res.render("urls_show", templateVars)
+});
+
+app.get("/urls", (req, res) => {
+  const templateVars = {
+    urls: urlDatabase 
+  }
+  res.render("urls_index", templateVars)
+});
+
+app.post("/urls", (req, res) => {
+  console.log(req.body);  // Log the POST request body to the console
+  res.send("Ok");         // Respond with 'Ok' (we will replace this)
 });
 
 app.get("/hello", (req, res) => {
